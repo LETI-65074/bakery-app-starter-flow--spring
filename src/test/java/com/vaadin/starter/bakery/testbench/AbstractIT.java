b76f894554b55e171e36047fd70279f458e91ea2
Zhe Sun <31067185+ZheSun88@users.noreply.github.com>
2025-03-12 15:01:25 +0200

    Update AbstractIT.java

--- src/test/java/com/vaadin/starter/bakery/testbench/AbstractIT.java
+++ src/test/java/com/vaadin/starter/bakery/testbench/AbstractIT.java
@@ -1,6 +1,7 @@
 package com.vaadin.starter.bakery.testbench;
 
 import org.junit.Rule;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 
 import com.vaadin.starter.bakery.testbench.elements.ui.LoginViewElement;
