28151169cf48d141126e1bde5bab095735ff3e36
LETI-65074 <snoye@iscte-iul.pt>
2025-09-25 00:13:39 +0100

    Add Javadoc documentation for Order class, including fields, constructors, and main methods

--- src/main/java/com/vaadin/starter/bakery/backend/data/entity/Order.java
+++ src/main/java/com/vaadin/starter/bakery/backend/data/entity/Order.java
@@ -10,11 +10,11 @@ import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.Index;
+import javax.persistence.NamedEntityGraphs;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.NamedAttributeNode;
 import javax.persistence.NamedEntityGraph;
-import javax.persistence.NamedEntityGraphs;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.OrderColumn;
@@ -26,147 +26,203 @@ import javax.validation.constraints.NotNull;
 import org.hibernate.annotations.BatchSize;
 
 import com.vaadin.starter.bakery.backend.data.OrderState;
-
-@Entity(name = "OrderInfo") // "Order" is a reserved word
-@NamedEntityGraphs({@NamedEntityGraph(name = Order.ENTITY_GRAPTH_BRIEF, attributeNodes = {
-		@NamedAttributeNode("customer"),
-		@NamedAttributeNode("pickupLocation")
-}),@NamedEntityGraph(name = Order.ENTITY_GRAPTH_FULL, attributeNodes = {
-		@NamedAttributeNode("customer"),
-		@NamedAttributeNode("pickupLocation"),
-		@NamedAttributeNode("items"),
-		@NamedAttributeNode("history")
-})})
+/**
+ * Represents an order in the bakery application.
+ * Stores information about due date, time, pickup location, customer, items, state, and history.
+ * <p>
+ * This entity is mapped to the database as "OrderInfo" to avoid reserved word conflicts.
+ * </p>
+ *
+ * @author LETI-65074
+ */
+@Entity(name = "OrderInfo")
+@NamedEntityGraphs({
+        @NamedEntityGraph(name = Order.ENTITY_GRAPTH_BRIEF, attributeNodes = {
+                @NamedAttributeNode("customer"),
+                @NamedAttributeNode("pickupLocation")
+        }),
+        @NamedEntityGraph(name = Order.ENTITY_GRAPTH_FULL, attributeNodes = {
+                @NamedAttributeNode("customer"),
+                @NamedAttributeNode("pickupLocation"),
+                @NamedAttributeNode("items"),
+                @NamedAttributeNode("history")
+        })
+})
 @Table(indexes = @Index(columnList = "dueDate"))
 public class Order extends AbstractEntity implements OrderSummary {
 
-	public static final String ENTITY_GRAPTH_BRIEF = "Order.brief";
-	public static final String ENTITY_GRAPTH_FULL = "Order.full";
+    /** Entity graph name for brief order info. */
+    public static final String ENTITY_GRAPTH_BRIEF = "Order.brief";
+    /** Entity graph name for full order info. */
+    public static final String ENTITY_GRAPTH_FULL = "Order.full";
 
-	@NotNull(message = "{bakery.due.date.required}")
-	private LocalDate dueDate;
+    /** Date when the order is due. */
+    @NotNull(message = "{bakery.due.date.required}")
+    private LocalDate dueDate;
 
-	@NotNull(message = "{bakery.due.time.required}")
-	private LocalTime dueTime;
+    /** Time when the order is due. */
+    @NotNull(message = "{bakery.due.time.required}")
+    private LocalTime dueTime;
 
-	@NotNull(message = "{bakery.pickup.location.required}")
-	@ManyToOne
-	private PickupLocation pickupLocation;
+    /** Location where the order will be picked up. */
+    @NotNull(message = "{bakery.pickup.location.required}")
+    @ManyToOne
+    private PickupLocation pickupLocation;
 
-	@NotNull
-	@OneToOne(cascade = CascadeType.ALL)
-	private Customer customer;
+    /** Customer who placed the order. */
+    @NotNull
+    @OneToOne(cascade = CascadeType.ALL)
+    private Customer customer;
 
-	@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER, orphanRemoval = true)
-	@OrderColumn
-	@JoinColumn
-	@BatchSize(size = 1000)
-	@NotEmpty
-	@Valid
-	private List<OrderItem> items;
-	@NotNull(message = "{bakery.status.required}")
-	private OrderState state;
+    /** List of items in the order. */
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER, orphanRemoval = true)
+    @OrderColumn
+    @JoinColumn
+    @BatchSize(size = 1000)
+    @NotEmpty
+    @Valid
+    private List<OrderItem> items;
 
+    /** Current state of the order. */
+    @NotNull(message = "{bakery.status.required}")
+    private OrderState state;
 
-	@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-	@OrderColumn
-	@JoinColumn
-	private List<HistoryItem> history;
+    /** History of changes for the order. */
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @OrderColumn
+    @JoinColumn
+    private List<HistoryItem> history;
 
-	public Order(User createdBy) {
-		this.state = OrderState.NEW;
-		setCustomer(new Customer());
-		addHistoryItem(createdBy, "Order placed");
-		this.items = new ArrayList<>();
-	}
+    /**
+     * Constructs a new Order with the given user as creator.
+     * Initializes state, customer, history, and items.
+     *
+     * @param createdBy the user who created the order
+     */
+    public Order(User createdBy) {
+        this.state = OrderState.NEW;
+        setCustomer(new Customer());
+        addHistoryItem(createdBy, "Order placed");
+        this.items = new ArrayList<>();
+    }
 
-	Order() {
-		// Empty constructor is needed by Spring Data / JPA
-	}
+    /**
+     * Protected no-arg constructor required by JPA.
+     */
+    Order() {
+        // Empty constructor is needed by Spring Data / JPA
+    }
 
-	public void addHistoryItem(User createdBy, String comment) {
-		HistoryItem item = new HistoryItem(createdBy, comment);
-		item.setNewState(state);
-		if (history == null) {
-			history = new LinkedList<>();
-		}
-		history.add(item);
-	}
+    /**
+     * Adds a history item to the order.
+     *
+     * @param createdBy the user who made the change
+     * @param comment   the comment describing the change
+     */
+    public void addHistoryItem(User createdBy, String comment) {
+        HistoryItem item = new HistoryItem(createdBy, comment);
+        item.setNewState(state);
+        if (history == null) {
+            history = new LinkedList<>();
+        }
+        history.add(item);
+    }
 
-	@Override
-	public LocalDate getDueDate() {
-		return dueDate;
-	}
+    /** @return the due date of the order */
+    @Override
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
 
-	public void setDueDate(LocalDate dueDate) {
-		this.dueDate = dueDate;
-	}
+    /** @param dueDate sets the due date of the order */
+    public void setDueDate(LocalDate dueDate) {
+        this.dueDate = dueDate;
+    }
 
-	@Override
-	public LocalTime getDueTime() {
-		return dueTime;
-	}
+    /** @return the due time of the order */
+    @Override
+    public LocalTime getDueTime() {
+        return dueTime;
+    }
 
-	public void setDueTime(LocalTime dueTime) {
-		this.dueTime = dueTime;
-	}
+    /** @param dueTime sets the due time of the order */
+    public void setDueTime(LocalTime dueTime) {
+        this.dueTime = dueTime;
+    }
 
-	@Override
-	public PickupLocation getPickupLocation() {
-		return pickupLocation;
-	}
+    /** @return the pickup location */
+    @Override
+    public PickupLocation getPickupLocation() {
+        return pickupLocation;
+    }
 
-	public void setPickupLocation(PickupLocation pickupLocation) {
-		this.pickupLocation = pickupLocation;
-	}
+    /** @param pickupLocation sets the pickup location */
+    public void setPickupLocation(PickupLocation pickupLocation) {
+        this.pickupLocation = pickupLocation;
+    }
 
-	@Override
-	public Customer getCustomer() {
-		return customer;
-	}
+    /** @return the customer who placed the order */
+    @Override
+    public Customer getCustomer() {
+        return customer;
+    }
 
-	public void setCustomer(Customer customer) {
-		this.customer = customer;
-	}
+    /** @param customer sets the customer for the order */
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
 
-	@Override
-	public List<OrderItem> getItems() {
-		return items;
-	}
+    /** @return the list of order items */
+    @Override
+    public List<OrderItem> getItems() {
+        return items;
+    }
 
-	public void setItems(List<OrderItem> items) {
-		this.items = items;
-	}
+    /** @param items sets the list of order items */
+    public void setItems(List<OrderItem> items) {
+        this.items = items;
+    }
 
-	public List<HistoryItem> getHistory() {
-		return history;
-	}
+    /** @return the history of the order */
+    public List<HistoryItem> getHistory() {
+        return history;
+    }
 
-	public void setHistory(List<HistoryItem> history) {
-		this.history = history;
-	}
+    /** @param history sets the history of the order */
+    public void setHistory(List<HistoryItem> history) {
+        this.history = history;
+    }
 
-	@Override
-	public OrderState getState() {
-		return state;
-	}
+    /** @return the current state of the order */
+    @Override
+    public OrderState getState() {
+        return state;
+    }
 
-	public void changeState(User user, OrderState state) {
-		boolean createHistory = this.state != state && this.state != null && state != null;
-		this.state = state;
-		if (createHistory) {
-			addHistoryItem(user, "Order " + state);
-		}
-	}
+    /**
+     * Changes the state of the order and adds a history item if the state changes.
+     *
+     * @param user  the user making the change
+     * @param state the new state
+     */
+    public void changeState(User user, OrderState state) {
+        boolean createHistory = this.state != state && this.state != null && state != null;
+        this.state = state;
+        if (createHistory) {
+            addHistoryItem(user, "Order " + state);
+        }
+    }
 
-	@Override
-	public String toString() {
-		return "Order{" + "dueDate=" + dueDate + ", dueTime=" + dueTime + ", pickupLocation=" + pickupLocation
-				+ ", customer=" + customer + ", items=" + items + ", state=" + state + '}';
-	}
+    /** @return string representation of the order */
+    @Override
+    public String toString() {
+        return "Order{" + "dueDate=" + dueDate + ", dueTime=" + dueTime + ", pickupLocation=" + pickupLocation
+                + ", customer=" + customer + ", items=" + items + ", state=" + state + '}';
+    }
 
-	@Override
-	public Integer getTotalPrice() {
-		return items.stream().map(i -> i.getTotalPrice()).reduce(0, Integer::sum);
-	}
+    /** @return the total price of all items in the order */
+    @Override
+    public Integer getTotalPrice() {
+        return items.stream().map(i -> i.getTotalPrice()).reduce(0, Integer::sum);
+    }
 }
